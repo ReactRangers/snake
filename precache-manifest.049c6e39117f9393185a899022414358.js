d02e416dba8e92711e13d817567f58b49e575163
Marcin Hagmajer <mhagmajer@gmail.com>
2018-10-09 20:23:15 +0200

    Updates

---
+++ precache-manifest.049c6e39117f9393185a899022414358.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/snake/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6f7f637fa69601b1d95bcc06f376a1d9",
+    "url": "/snake/static/media/cherries.6f7f637f.svg"
+  },
+  {
+    "revision": "da1ecade61dd97107e27",
+    "url": "/snake/static/js/runtime~main.da1ecade.js"
+  },
+  {
+    "revision": "9c7721921873fb7d5568",
+    "url": "/snake/static/js/main.9c772192.chunk.js"
+  },
+  {
+    "revision": "fed942a78602f2634ec3",
+    "url": "/snake/static/js/1.fed942a7.chunk.js"
+  },
+  {
+    "revision": "9c7721921873fb7d5568",
+    "url": "/snake/static/css/main.62a6ad7e.chunk.css"
+  },
+  {
+    "revision": "938f7eee57e16aa7c8e394dd2b536729",
+    "url": "/snake/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/snake-react/precache-manifest.8bb0c6d708bbd152f3e3e339f9bdce56.js"
+  "/snake/precache-manifest.049c6e39117f9393185a899022414358.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/snake-react/index.html", {
+workbox.routing.registerNavigationRoute("/snake/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
